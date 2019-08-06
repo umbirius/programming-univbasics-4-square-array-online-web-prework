c797e2ee491054eaf48230267c71423219a872c5
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-06 21:35:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
   counter =0
   while counter<array.length 
     array[counter]= array[counter]*array[counter]
-    puts array [counter]
+    p array [counter]
     counter+=1 
   end 
 end
