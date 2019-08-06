48650e82fc3811a73bb1f36aa3a17d99e2603baf
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-06 21:34:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
-counter =0
+
 array = [2,3,5,7]
 
 def square_array(array)
+  counter =0
   while counter<array.length 
   array[counter]= array[counter]*array[counter]
   puts array [counter]
   counter+=1 
+end 
 end
