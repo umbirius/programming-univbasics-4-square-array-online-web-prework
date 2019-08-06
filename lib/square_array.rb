c0dcbf8357f44b89fd5e4f29d81ec50e0f315fb7
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-06 21:39:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,8 +4,9 @@ numbers = [1,2,3]
 def square_array(numbers)
   counter =0
   while counter<numbers.length 
-    new_numbers[counter]= numbers[counter]*numbers[counter]
+    numbers[counter]= numbers[counter]*numbers[counter]
     counter+=1 
   end 
-  puts new_numbers
+  
+  p numbers
 end
